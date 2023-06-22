b0ad2359578511d35c602bbc1bfadd5736ff84b2
Dany Bergquist <dany.bergquist@mable.com.au>
2023-06-22 12:15:45 +1000

    Pass amount to credit card processor

--- app/collection_processor.rb
+++ app/collection_processor.rb
@@ -1,5 +1,11 @@
 class CollectionProcessor
   def process(amount)
-    raise 'Amount must be above zero' unless amount > 0
+    raise 'Amount must be above zero' unless amount.positive?
+
+    service = CreditCardCollectionService.new
+
+    service.process(amount)
   end
 end
+
+class CreditCardCollectionService; end
--- spec/collection_processor_spec.rb
+++ spec/collection_processor_spec.rb
@@ -2,9 +2,23 @@ require './app/collection_processor'
 
 RSpec.describe CollectionProcessor do
   subject(:processor) { CollectionProcessor.new }
-  it 'raises an error unless amount above zero' do
-    amount = 0
+  describe '#process' do
+    let(:service) { double(CreditCardCollectionService, process: nil) }
 
-    expect { processor.process(amount) }.to raise_error('Amount must be above zero')
+    before do
+      allow(CreditCardCollectionService).to receive(:new).and_return(service)
+    end
+
+    it 'raises an error unless amount above zero' do
+      amount = 0
+
+      expect { processor.process(amount) }.to raise_error('Amount must be above zero')
+    end
+
+    it 'passes amount on to CreditCardCollectionService' do
+      amount = 100
+      processor.process(amount)
+      expect(service).to have_received(:process).with(amount)
+    end
   end
 end
